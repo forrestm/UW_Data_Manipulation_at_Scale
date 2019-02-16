1f3807e6ef51f7273454050aced399cc198841d6
Forrest <for290@gmail.com>
2019-02-16 14:21:07 -0600

    Sorting Text
    Got the tweets to be sorted into a smaller dictionary and trashed deleted tweets.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -2,17 +2,18 @@ import sys
 import json
 
 def hw():
-    print 'Hello, world!'
+    print('Hello, world!')
 
 def lines(fp):
-    print str(len(fp.readlines()))
+    print(str(len(fp.readlines())))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    print('We out here')
+    # sent_file = open(sys.argv[1])
+    # tweet_file = open(sys.argv[2])
+    # hw()
+    # lines(sent_file)
+    # lines(tweet_file)
 
 afinnfile = open("AFINN-111.txt")
 scores = {} # initialize an empty dictionary
@@ -21,6 +22,14 @@ for line in afinnfile:
   scores[term] = int(score)  # Convert the score to an integer.
 
 # print(scores.items()) # Print every (term, score) pair in the dictionary
+
+tweetcleanup = {}
+tweets = {}
+# for line in open('output.json', 'r'):
+for line in open('output.json', 'r'):
+    tweets.update(json.loads(line))
+    if 'id' in tweets:
+        tweetcleanup.update({tweets['id']:tweets['text']})
 
 if __name__ == '__main__':
     main()
